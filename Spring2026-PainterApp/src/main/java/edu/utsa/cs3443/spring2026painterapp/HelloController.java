7fcdac09aa82fac030db3da697ec6ae800ff4ced
pfv610 <mitra.bokaeihosseini@utsa.edu>
2026-03-27 09:43:03 -0500

    implemented the fxml file for color app

--- Spring2026-PainterApp/src/main/java/edu/utsa/cs3443/spring2026painterapp/HelloController.java
+++ Spring2026-PainterApp/src/main/java/edu/utsa/cs3443/spring2026painterapp/HelloController.java
@@ -1,14 +1,74 @@
 package edu.utsa.cs3443.spring2026painterapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private RadioButton blackRadioButton;
+
+    @FXML
+    private RadioButton blueRadioButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private ToggleGroup colorToggleGroup;
+
+    @FXML
+    private Pane drawingAreaPane;
+
+    @FXML
+    private RadioButton greenRadioButton;
+
+    @FXML
+    private RadioButton largeRadioButton;
+
+    @FXML
+    private RadioButton mediumRadioButton;
+
+    @FXML
+    private RadioButton redRadioButton;
+
+    @FXML
+    private ToggleGroup sizeToggleGroup;
+
+    @FXML
+    private RadioButton smallRadioButton;
+
+    @FXML
+    private Button undoButton;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+
     }
+
+    @FXML
+    void colorRadioButtonSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sizeRadioButtonSelected(ActionEvent event) {
+
+    }
+
+    @FXML
+    void undoButtonPressed(ActionEvent event) {
+
+    }
+
 }
